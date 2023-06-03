3a667844261919772bd93e29590c9e72173fb6a9
Fernando Diego Martins Coelho <fernandomartinsdk@gmail.com>
2023-06-03 17:23:42 -0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,110 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ScoreBoard :winCount='this.winCount' :loseCount="this.loseCount" />
+    <template v-if="this.question">
+      <h1 v-html="this.question"></h1>
+
+      <template v-for='(answer,index) in this.answers' :key='index'>
+        <input :disabled="this.answerSubmitted" type="radio" name="options" :value="answer" v-model="this.chosen_answer">
+        <label v-html="answer"></label>
+        <br>
+      </template>
+      
+      <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
+    
+      <section class="result" v-if="this.answerSubmitted">
+        <h4 v-if="this.chosen_answer == this.correctAnswer" v-html="'✔️ Você acertou!!!'"></h4>
+        <h4 v-else v-html="'❌ Você errou, que pena! A resposta certa seria: '+ this.correctAnswer +' !'"></h4>
+        <button @click="this.getNewQuestion()" class="send" type="button">Next question</button>
+      </section>
+      
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScoreBoard from './components/ScoreBoard.vue';
 
-export default {
+export default { 
+
   name: 'App',
+  data(){
+    return{
+      question:undefined,
+      incorrectAnswers:undefined,
+      correctAnswer:undefined,
+      chosen_answer:undefined,
+      answerSubmitted:false,
+      winCount:0,
+      loseCount:0
+    }
+  },
+  computed:{
+    answers(){
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+        answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+        return answers;
+    }
+  },
+  methods:{
+    submitAnswer(){
+      if (!this.chosen_answer) {
+        alert('Escolha uma das opções')
+      }else{
+        this.answerSubmitted = true;
+        if (this.chosen_answer == this.correctAnswer) {
+          this.winCount++;
+        }else{
+          this.loseCount++;
+        }
+      }
+    },
+    getNewQuestion(){
+      this.answerSubmitted = false;
+      this.chosen_answer = undefined;
+      this.question = undefined;
+
+      this.axios.get('https://opentdb.com/api.php?amount=1').then((response) => {
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswer = response.data.results[0]['correct_answer'];
+    });
+    }
+  },
+  created(){
+    this.getNewQuestion(); 
+  },
   components: {
-    HelloWorld
-  }
+    ScoreBoard
+}
 }
 </script>
 
 <style lang="scss">
 #app {
+  max-width: 960px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin: 60px auto;
+  
+  input[type=radio]{
+    margin: 12px 4px;
+  }
+
+  button.send{
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
+
 }
 </style>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <section class="score">
+            Player <span>{{ this.winCount }}</span> X <span>{{ this.loseCount }}</span> Computer
+        </section>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:[
+            'winCount',
+            'loseCount'
+        ]
+    }
+</script>
+
+<style lang="scss">
+    section.score{
+        border-bottom: 1px solid black;
+        padding: 24px;
+        font-size: 18px;
+    }
+    section{
+        display: block;
+    }
+    section.score span{
+        padding: 8px;
+        font-weight: bold;
+        border: 1px solid black;
+    }
+</style>
